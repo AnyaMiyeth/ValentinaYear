22d5baa903839d9c1cf7fd600884a09d5d8f7e6e
anyamiyeth <anyamiyeth@byasystems.com.co>
2020-04-02 15:44:45 -0500

    Agregar archivos de proyecto.

---
+++ ValentinaYear/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ValentinaYear
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateTime fecha = dateTimePicker1.Value;
+           var Edad = DateTime.Now.Year-fecha.Year ;
+            textBox2.Text = Edad.ToString();
+        }
+    }
+}
